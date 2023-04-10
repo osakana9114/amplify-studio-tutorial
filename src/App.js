63c122b68269b1d3c3f5c294d10bfd118dcf71ab
Yuka Ajisaka <47623066+osakana9114@users.noreply.github.com>
2023-04-10 13:05:53 +0900

    Adjusted margins

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import { NewHomes, NavBar, MarketingFooter } from './ui-components'
+import {NewHomes, NavBar, MarketingFooter, HeroLayout2} from './ui-components'
 
 function App() {
   return (
     <div className="App">
       <NavBar width={"100vw"}/>
+      <HeroLayout2 width={"100vw"} marginBottom={30}/>
       <NewHomes isPaginated itemsPerPage={3}/>
-      <MarketingFooter width={"100vw"}/>
+      <MarketingFooter width={"100vw"} marginTop={50}/>
     </div>
   );
 }
